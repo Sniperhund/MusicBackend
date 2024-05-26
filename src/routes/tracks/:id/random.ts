c1a5344238d27de63a4f300cdb97de9b087b6c58
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-05-26 23:26:17 +0200

    Fixed random endpoint

--- src/routes/tracks/:id/random.ts
+++ src/routes/tracks/:id/random.ts
@@ -1,29 +1,46 @@
 import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
-import { Album, Artist, Track } from "../../../schemas"
+import { Album, Artist, Track, Genre } from "../../../schemas"
 import authenticate from "../../../middleware/auth"
+import mongoose from "mongoose"
 
 export default (express: Application) =>
 	<Resource>{
 		middleware: [authenticate],
 		get: async (request: Request, response: Response) => {
-			let tracks: any[] = []
-
-			//! BUG: It doesn't work with filters yet.
-			const count = await Track.countDocuments({
-				genre: request.params.id,
-			})
-
-			const num: number = request.query.limit
+			const genreId = request.params.id
+			const limit = request.query.limit
 				? parseInt(request.query.limit as string)
 				: 10
 
-			for (let i = 0; i < num; i++) {
-				let random = Math.floor(Math.random() * count)
-
-				tracks.push(await Track.findOne().skip(random))
+			const genre = await Genre.findById(genreId)
+			if (!genre) {
+				return response.status(404).json({ error: "Genre not found" })
 			}
 
-			response.status(200).json(tracks)
+			// Aggregate and get random tracks
+			const randomTracks = await Track.aggregate([
+				{
+					$lookup: {
+						from: "albums",
+						localField: "album",
+						foreignField: "_id",
+						as: "albumInfo",
+					},
+				},
+				{
+					$unwind: "$albumInfo",
+				},
+				{
+					$match: {
+						"albumInfo.genres": new mongoose.Types.ObjectId(
+							genreId
+						),
+					},
+				},
+				{ $sample: { size: limit } },
+			])
+
+			return response.status(200).json(randomTracks)
 		},
 	}
