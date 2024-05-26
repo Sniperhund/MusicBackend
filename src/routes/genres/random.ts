b33b87e4a1afc23fec957c809af5779a2a7ce382
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-05-27 00:02:19 +0200

    Updated genres/random to have a exclude query param

--- src/routes/genres/random.ts
+++ src/routes/genres/random.ts
@@ -8,9 +8,27 @@ export default (express: Application) =>
 	<Resource>{
 		middleware: [authenticate],
 		get: async (request: Request, response: Response) => {
-			const randomGenre = await Genre.aggregate([
-				{ $sample: { size: 1 } },
-			])
+			const excludeIds = request.query.exclude
+				? (request.query.exclude as string)
+						.split(",")
+						.map((id) => new mongoose.Types.ObjectId(id))
+				: []
+
+			const pipeline = []
+
+			console.log(excludeIds)
+
+			// Add match stage to exclude the specified genres if excludeIds are provided
+			if (excludeIds.length > 0) {
+				pipeline.push({
+					$match: { _id: { $nin: excludeIds } },
+				})
+			}
+
+			// Add sample stage to get a random genre
+			pipeline.push({ $sample: { size: 1 } })
+
+			const randomGenre = await Genre.aggregate(pipeline)
 
 			if (randomGenre.length === 0) {
 				return response.status(404).json({ error: "No genres found" })
