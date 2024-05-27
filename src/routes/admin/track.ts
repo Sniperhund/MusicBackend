60963ba7c3adf9eacbf7d8076199b0f87330878f
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-05-27 12:07:27 +0200

    Updated track schema to have duration in seconds

--- src/routes/admin/track.ts
+++ src/routes/admin/track.ts
@@ -2,17 +2,30 @@ import { Application, Request, Response, response } from "express"
 import { Resource } from "express-automatic-routes"
 import { trackUpload } from "../../middleware/upload"
 import { Track } from "../../schemas"
+// @ts-ignore
+import getMP3Duration from "get-mp3-duration"
+import auth from "../../middleware/auth"
+import * as fs from "fs"
+
+const uploadDir = process.env.UPLOAD_DIR || "public"
 
 export default (express: Application) =>
 	<Resource>{
 		post: {
-			middleware: [trackUpload.single("file")],
+			middleware: [auth, trackUpload.single("file")],
 			handler: (request: Request, response: Response) => {
+				const fileLocation =
+					uploadDir + "/track/" + request.file?.filename
+
+				const buffer = fs.readFileSync(fileLocation)
+				const duration = Math.floor(getMP3Duration(buffer) / 1000)
+
 				let track = new Track({
 					name: request.body.name,
 					album: request.body.album,
 					artist: request.body.artist,
 					audioFile: request.file?.filename,
+					durationInSeconds: duration,
 				})
 
 				track.save()
--- src/schemas.ts
+++ src/schemas.ts
@@ -1,91 +1,92 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 // User Schema
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  accessToken: String,
-  accessTokenExpire: {
-    type: Date,
-    default: () => Date.now() + 60 * 60 * 24 * 7 * 1000, // 7 days
-  },
-  refreshToken: String,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  verifyToken: String,
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  savedTracks: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Track",
-    },
-  ],
-});
+	name: String,
+	email: String,
+	password: String,
+	role: {
+		type: String,
+		enum: ["user", "admin"],
+		default: "user",
+	},
+	accessToken: String,
+	accessTokenExpire: {
+		type: Date,
+		default: () => Date.now() + 60 * 60 * 24 * 7 * 1000, // 7 days
+	},
+	refreshToken: String,
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	verifyToken: String,
+	verified: {
+		type: Boolean,
+		default: false,
+	},
+	savedTracks: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Track",
+		},
+	],
+})
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema)
 
 // Genres Schema
 const genres = new mongoose.Schema({
-  name: String,
-});
+	name: String,
+})
 
-export const Genre = mongoose.model("Genre", genres);
+export const Genre = mongoose.model("Genre", genres)
 
 // Track Schema
 const track = new mongoose.Schema({
-  name: String,
-  album: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Album",
-  },
-  artist: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Artist",
-  },
-  audioFile: String,
-});
+	name: String,
+	album: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Album",
+	},
+	artist: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Artist",
+	},
+	audioFile: String,
+	durationInSeconds: Number,
+})
 
-track.index({ name: "text" });
+track.index({ name: "text" })
 
-export const Track = mongoose.model("Track", track);
+export const Track = mongoose.model("Track", track)
 
 // Album Schema
 const album = new mongoose.Schema({
-  name: String,
-  artist: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Artist",
-  },
-  cover: String,
-  genres: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Genre",
-    },
-  ],
-});
+	name: String,
+	artist: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Artist",
+	},
+	cover: String,
+	genres: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Genre",
+		},
+	],
+})
 
-album.index({ name: "text" });
+album.index({ name: "text" })
 
-export const Album = mongoose.model("Album", album);
+export const Album = mongoose.model("Album", album)
 
 // Artist Schema
 const artist = new mongoose.Schema({
-  name: String,
-  cover: String,
-});
+	name: String,
+	cover: String,
+})
 
-artist.index({ name: "text" });
+artist.index({ name: "text" })
 
-export const Artist = mongoose.model("Artist", artist);
+export const Artist = mongoose.model("Artist", artist)
