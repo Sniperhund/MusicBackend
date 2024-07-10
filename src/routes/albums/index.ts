8e5bd573440b647dc1847e2aee0f074d770c3dfe
Lucas Täkker <lucastakker@gmail.com>
2024-07-10 02:38:50 +0200

    chore: Add error handling and validation to get albums by ids route

--- src/routes/albums/index.ts
+++ src/routes/albums/index.ts
@@ -1,28 +1,52 @@
 import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
 import { Album } from "../../schemas"
+import auth from "../../middleware/auth"
+import { upload } from "../../middleware/upload"
+import mongoose from "mongoose"
+import { log } from "../../utils/logger"
 
 export default (express: Application) =>
 	<Resource>{
+		middleware: [upload.none(), auth],
 		get: async (request: Request, response: Response) => {
-			let errorHappened = false
-
 			if (!request.body.ids) {
-				response.status(422).json({ message: "ids is required" })
-				errorHappened = true
+				return response.status(400).json({
+					status: "error",
+					message: "Ids are required",
+				})
+			}
+
+			if (request.body.ids && !Array.isArray(request.body.ids)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Ids must be an array",
+				})
+			}
+
+			for (let id of request.body.ids) {
+				if (!mongoose.Types.ObjectId.isValid(id)) {
+					return response.status(400).json({
+						status: "error",
+						message: "Invalid id",
+					})
+				}
 			}
 
 			const albums = await Album.find({
 				_id: { $in: request.body.ids },
-			})
-				.populate("artist")
-				.catch((error) => {
-					response.status(500).json({ message: error.message })
-					errorHappened = true
+			}).populate("artist")
+
+			if (albums.length == 0) {
+				return response.status(404).json({
+					status: "error",
+					message: "No albums found",
 				})
+			}
 
-			if (errorHappened) return
-
-			response.status(200).json(albums)
+			response.status(200).json({
+				status: "ok",
+				response: albums,
+			})
 		},
 	}
