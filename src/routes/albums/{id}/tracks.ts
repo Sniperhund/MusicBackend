7046f0ba479c1b3eb55e1c64436fc225787645f3
Lucas Täkker <lucastakker@gmail.com>
2024-07-10 02:55:28 +0200

    chore: Refactor album route to use auth middleware and handle invalid album ID

--- src/routes/albums/{id}/tracks.ts
+++ src/routes/albums/{id}/tracks.ts
@@ -1,32 +1,58 @@
 import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
 import { Album, Track } from "../../../schemas"
-import authenticate from "../../../middleware/auth"
+import auth from "../../../middleware/auth"
+import mongoose from "mongoose"
 
 export default (express: Application) =>
 	<Resource>{
-		middleware: [authenticate],
+		middleware: [auth],
 		get: async (request: Request, response: Response) => {
+			if (!mongoose.Types.ObjectId.isValid(request.params.id)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid id",
+				})
+			}
+
+			const limit = request.query.limit
+				? parseInt(request.query.limit as string)
+				: 0
+
+			if (isNaN(limit) || limit < 0) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid limit",
+				})
+			}
+
+			const skip = request.query.offset
+				? parseInt(request.query.offset as string)
+				: 0
+
+			if (isNaN(skip) || skip < 0) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid offset",
+				})
+			}
+
 			const tracks = await Track.find({ album: request.params.id })
-				.limit(
-					request.query.limit
-						? parseInt(request.query.limit as string)
-						: 0
-				)
-				.skip(
-					request.query.offset
-						? parseInt(request.query.offset as string)
-						: 0
-				)
+				.limit(limit)
+				.skip(skip)
 				.populate("album")
 				.populate("artist")
 
-			if (!tracks)
-				return response.status(400).json({
+			if (tracks.length === 0) {
+				return response.status(404).json({
 					status: "error",
-					message: "No tracks found for this album",
+					message: "No tracks found",
 				})
+			}
 
-			response.status(200).json(tracks)
+			response.status(200).json({
+				status: "ok",
+				response: tracks,
+			})
 		},
 	}
