3178c6d485fb39f40576504ade09f6d578493e48
Lucas Täkker <lucastakker@gmail.com>
2024-07-10 02:05:47 +0200

    Refactor genre route to add authentication and file upload middleware

--- src/routes/admin/genre.ts
+++ src/routes/admin/genre.ts
@@ -1,18 +1,35 @@
 import { Application, Request, Response, response } from "express"
 import { Resource } from "express-automatic-routes"
 import { Genre } from "../../schemas"
+import auth from "../../middleware/auth"
+import { upload } from "../../middleware/upload"
 
 export default (express: Application) =>
 	<Resource>{
 		post: {
-			handler: (request: Request, response: Response) => {
+			middleware: [upload.none(), auth],
+			handler: async (request: Request, response: Response) => {
+				if (!request.body.name) {
+					return response.status(400).json({
+						status: "error",
+						message: "Name is required",
+					})
+				}
+
+				if (request.body.user.role != "admin") {
+					return response.status(401).json({
+						status: "error",
+						message: "Unauthorized",
+					})
+				}
+
 				let genre = new Genre({
 					name: request.body.name,
 				})
 
-				genre.save()
+				await genre.save()
 
-				response.status(200).json({
+				response.status(201).json({
 					status: "ok",
 					response: genre,
 				})
