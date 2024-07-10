46be2b69fa65c681d33d4b4d0fc3ca68baf06033
Lucas Täkker <lucastakker@gmail.com>
2024-07-10 02:05:16 +0200

    Added express-async-errors package to improve error handling in async routes.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,18 @@ import mongoose from "mongoose"
 import cors from "cors"
 import { config } from "dotenv"
 import { log } from "./utils/logger"
+import logError from "./utils/logError"
+import cleanFile from "./utils/cleanFile"
 const swaggerUi = require("swagger-ui-express")
 const fs = require("fs")
 const YAML = require("yaml")
+require("express-async-errors")
 
 config()
 
 let app: Application = express()
+
+const uploadDir = process.env.UPLOAD_DIR || "public"
 
 async function main() {
 	if (!process.env.MONGODB_URL) throw new Error("MONGODB_URL is not defined")
@@ -21,6 +26,7 @@ async function main() {
 
 	app.use(cors())
 	app.use(express.static(process.env.UPLOAD_DIR))
+	app.use(express.urlencoded({ extended: true }))
 	app.use(express.json())
 
 	const options = { explorer: true }
@@ -42,6 +48,27 @@ async function main() {
 
 	autoroutes(app, {
 		dir: "./routes/",
+	})
+
+	app.use((error: any, request: any, response: any, next: any) => {
+		if (error) {
+			const id = logError(error.message)
+
+			if (request.file) {
+				const fileLocation = request.file?.path
+
+				cleanFile(fileLocation)
+			}
+
+			return response.status(500).json({
+				status: "error",
+				errorId: id,
+			})
+		}
+
+		return response.status(500).json({
+			status: "error",
+		})
 	})
 
 	app.listen(port, () => {
