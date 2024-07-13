991133848ff09193cd85e718d3025eee5c117d7b
Lucas Täkker <lucastakker@gmail.com>
2024-07-14 00:36:36 +0200

    chore: Refractor refresh to be more robust

--- src/routes/auth/refresh.ts
+++ src/routes/auth/refresh.ts
@@ -2,41 +2,35 @@ import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
 import { User } from "../../schemas"
 import { v4 as uuidv4 } from "uuid"
+import { upload } from "../../middleware/upload"
 
 export default (express: Application) =>
 	<Resource>{
+		middleware: [upload.none()],
 		post: async (request: Request, response: Response) => {
-			try {
-				let user = await User.findOne({
-					refreshToken: request.body.refreshToken,
+			let user = await User.findOne({
+				refreshToken: request.body.refreshToken,
+			})
+
+			if (!user)
+				return response.status(400).json({
+					status: "error",
+					message: "The refresh token is invalid",
 				})
 
-				if (!user)
-					return response.status(404).json({
-						status: "error",
-						message: "The refresh token is invalid",
-					})
+			user.accessToken = uuidv4()
+			user.accessTokenExpire = new Date(
+				Date.now() + 60 * 60 * 24 * 7 * 1000
+			)
 
-				user.accessToken = uuidv4()
-				user.accessTokenExpire = new Date(
-					Date.now() + 60 * 60 * 24 * 7 * 1000
-				)
+			await user.save()
 
-				await user.save()
-
-				response.status(200).json({
-					status: "ok",
+			response.status(200).json({
+				status: "ok",
+				response: {
 					accessToken: user.accessToken,
 					accessTokenExpire: user.accessTokenExpire,
-				})
-			} catch (error) {
-				if (typeof error === "object" && error && "message" in error) {
-					return response.status(500).json({ message: error.message })
-				} else {
-					return response
-						.status(500)
-						.json({ message: "An unknown error occurred" })
-				}
-			}
+				},
+			})
 		},
 	}
