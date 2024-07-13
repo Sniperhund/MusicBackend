4b17686e09ed8aab7dd6650834269c1f4c3cc503
Lucas Täkker <lucastakker@gmail.com>
2024-07-14 01:21:46 +0200

    chore: Update tracks route to handle invalid ids and limit values

--- src/routes/tracks/{id}/random.ts
+++ src/routes/tracks/{id}/random.ts
@@ -1,25 +1,52 @@
 import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
-import { Album, Artist, Track, Genre } from "../../../schemas"
-import authenticate from "../../../middleware/auth"
+import { Track, Genre } from "../../../schemas"
+import auth from "../../../middleware/auth"
 import mongoose from "mongoose"
+import { upload } from "../../../middleware/upload"
 
 export default (express: Application) =>
 	<Resource>{
-		middleware: [authenticate],
+		middleware: [upload.none(), auth],
 		get: async (request: Request, response: Response) => {
+			if (!mongoose.Types.ObjectId.isValid(request.params.id)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid id",
+				})
+			}
+
 			const genreId = request.params.id
+
+			if (!mongoose.Types.ObjectId.isValid(genreId)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid genre id",
+				})
+			}
+
 			const limit = request.query.limit
 				? parseInt(request.query.limit as string)
 				: 10
 
+			if (isNaN(limit) || limit < 0) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid limit",
+				})
+			}
+
 			const genre = await Genre.findById(genreId)
+
 			if (!genre) {
-				return response.status(404).json({ error: "Genre not found" })
+				return response.status(400).json({
+					status: "error",
+					message: "Genre not found",
+				})
 			}
 
 			// Aggregate and get random tracks
-			const randomTracks = await Track.aggregate([
+			const pipeline = [
 				{
 					$lookup: {
 						from: "albums",
@@ -48,8 +75,20 @@ export default (express: Application) =>
 					},
 				},
 				{ $sample: { size: limit } },
-			])
+			]
 
-			return response.status(200).json(randomTracks)
+			const randomTracks = await Track.aggregate(pipeline)
+
+			if (randomTracks.length === 0) {
+				return response.status(404).json({
+					status: "error",
+					message: "No tracks found",
+				})
+			}
+
+			return response.status(200).json({
+				status: "ok",
+				response: randomTracks,
+			})
 		},
 	}
