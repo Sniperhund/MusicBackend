fb6e5ff2fb4d49eb1fa03bfc3aac9741765e3926
Lucas Täkker <lucastakker@gmail.com>
2024-07-13 23:47:25 +0200

    chore: Refactor artist route to use auth middleware and handle invalid artist ID

--- src/routes/artists/{id}.ts
+++ src/routes/artists/{id}.ts
@@ -2,23 +2,31 @@ import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
 import { Artist } from "../../schemas"
 import authenticate from "../../middleware/auth"
+import mongoose from "mongoose"
 
 export default (express: Application) =>
 	<Resource>{
 		middleware: [authenticate],
 		get: async (request: Request, response: Response) => {
-			try {
-				const artist = await Artist.findById(request.params.id)
-
-				response.status(200).json(artist)
-			} catch (error) {
-				if (typeof error === "object" && error && "message" in error) {
-					return response.status(500).json({ message: error.message })
-				} else {
-					return response
-						.status(500)
-						.json({ message: "An unknown error occurred" })
-				}
+			if (!mongoose.Types.ObjectId.isValid(request.params.id)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid id",
+				})
 			}
+
+			const artist = await Artist.findById(request.params.id)
+
+			if (!artist) {
+				return response.status(404).json({
+					status: "error",
+					message: "Artist not found",
+				})
+			}
+
+			response.status(200).json({
+				status: "ok",
+				response: artist,
+			})
 		},
 	}
