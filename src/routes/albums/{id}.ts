186123964009be2e5d2f768063ddc21cffda273c
Lucas Täkker <lucastakker@gmail.com>
2024-07-13 23:47:21 +0200

    Refactor album route to use auth middleware and handle invalid album ID

--- src/routes/albums/{id}.ts
+++ src/routes/albums/{id}.ts
@@ -3,10 +3,11 @@ import { Resource } from "express-automatic-routes"
 import { Album } from "../../schemas"
 import auth from "../../middleware/auth"
 import mongoose from "mongoose"
+import { upload } from "../../middleware/upload"
 
 export default (express: Application) =>
 	<Resource>{
-		middleware: [auth],
+		middleware: [upload.none(), auth],
 		get: async (request: Request, response: Response) => {
 			if (!mongoose.Types.ObjectId.isValid(request.params.id)) {
 				return response.status(400).json({
