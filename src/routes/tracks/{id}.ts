0266cf5880ace57aaa6b9767646d99301f93940c
Lucas Täkker <lucastakker@gmail.com>
2024-07-14 01:15:43 +0200

    chore: Update tracks route to handle invalid ids and return appropriate error messages

--- src/routes/tracks/{id}.ts
+++ src/routes/tracks/{id}.ts
@@ -1,26 +1,35 @@
 import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
 import { Track } from "../../schemas"
-import authenticate from "../../middleware/auth"
+import auth from "../../middleware/auth"
+import { upload } from "../../middleware/upload"
+import mongoose from "mongoose"
 
 export default (express: Application) =>
 	<Resource>{
-		middleware: [authenticate],
+		middleware: [upload.none(), auth],
 		get: async (request: Request, response: Response) => {
-			try {
-				const track = await Track.findById(request.params.id)
-					.populate("album")
-					.populate("artist")
-
-				response.status(200).json(track)
-			} catch (error) {
-				if (typeof error === "object" && error && "message" in error) {
-					return response.status(500).json({ message: error.message })
-				} else {
-					return response
-						.status(500)
-						.json({ message: "An unknown error occurred" })
-				}
+			if (!mongoose.Types.ObjectId.isValid(request.params.id)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid id",
+				})
 			}
+
+			const track = await Track.findById(request.params.id)
+				.populate("album")
+				.populate("artist")
+
+			if (!track) {
+				return response.status(404).json({
+					status: "error",
+					message: "Track not found",
+				})
+			}
+
+			response.status(200).json({
+				status: "ok",
+				response: track,
+			})
 		},
 	}
