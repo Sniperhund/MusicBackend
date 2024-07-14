5065a6ca7b2373ab5a1997dbe0a664b10505b069
Lucas Täkker <lucastakker@gmail.com>
2024-07-14 16:37:07 +0200

    chore: Refractor password to be more robust

--- src/routes/user/password.ts
+++ src/routes/user/password.ts
@@ -1,48 +1,80 @@
 import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
 import { User } from "../../schemas"
-import authenticate from "../../middleware/auth"
-import { Resend } from "resend"
-import { v4 as uuidv4 } from "uuid"
+import auth from "../../middleware/auth"
+import { upload } from "../../middleware/upload"
+import validator from "validator"
 
 export default (express: Application) =>
 	<Resource>{
-		middleware: [authenticate],
+		middleware: [upload.none(), auth],
 		post: async (request: Request, response: Response) => {
-			try {
-				let user = await User.findOne({
-					accessToken: request.body.user.accessToken,
+			if (!request.body.oldPassword) {
+				return response.status(400).json({
+					status: "error",
+					message: "Old password is required",
 				})
-
-				if (!user)
-					return response.status(400).json({
-						status: "error",
-						message: "User not found",
-					})
-
-				if (!request.body.oldPassword || !request.body.newPassword)
-					return response.status(422).json({
-						status: "error",
-						message: "One or more fields are missing",
-					})
-
-				if (user.password != request.body.oldPassword)
-					return response.status(400).json({
-						status: "error",
-						message: "Wrong password",
-					})
-
-				user.password = request.body.newPassword
-
-				await user.save()
-			} catch (error) {
-				if (typeof error === "object" && error && "message" in error) {
-					return response.status(500).json({ message: error.message })
-				} else {
-					return response
-						.status(500)
-						.json({ message: "An unknown error occurred" })
-				}
 			}
+
+			if (!request.body.newPassword) {
+				return response.status(400).json({
+					status: "error",
+					message: "New password is required",
+				})
+			}
+
+			/**
+			 * minLength: 8
+			 * minLowercase: 1
+			 * minUppercase: 1
+			 * minNumbers: 1
+			 * minSymbols: 1
+			 * returnScore: false
+			 * pointsPerUnique: 1
+			 * pointsPerRepeat: 0.5
+			 * pointsForContainingLower: 10
+			 * pointsForContainingUpper: 10
+			 * pointsForContainingNumber: 10
+			 * pointsForContainingSymbol: 10
+			 */
+			if (!validator.isStrongPassword(request.body.password)) {
+				return response.status(400).json({
+					status: "error",
+					message: "New password is too weak",
+				})
+			}
+
+			let user = await User.findOne({
+				accessToken: request.body.user.accessToken,
+			})
+
+			if (!user) {
+				return response.status(400).json({
+					status: "error",
+					message: "User not found",
+				})
+			}
+
+			if (user.password != request.body.oldPassword) {
+				return response.status(400).json({
+					status: "error",
+					message: "Password is incorrect",
+				})
+			}
+
+			if (request.body.oldPassword == request.body.newPassword) {
+				return response.status(400).json({
+					status: "error",
+					message: "New password must be different from old password",
+				})
+			}
+
+			user.password = request.body.newPassword
+
+			await user.save()
+
+			return response.status(200).json({
+				status: "ok",
+			})
 		},
 	}
