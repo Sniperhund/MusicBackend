01f0deca1e31aba6cfaa5503196708ab2a8b42d9
Lucas Täkker <lucastakker@gmail.com>
2024-07-14 16:50:13 +0200

    chore: Refractor user/albums/contains to be more robust

--- src/routes/user/albums/contains.ts
+++ src/routes/user/albums/contains.ts
@@ -1,37 +1,41 @@
 import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
-import authenticate from "../../../middleware/auth"
+import auth from "../../../middleware/auth"
 import { Album, User } from "../../../schemas"
+import { upload } from "../../../middleware/upload"
+import mongoose from "mongoose"
 
 export default (express: Application) =>
 	<Resource>{
-		middleware: [authenticate],
+		middleware: [upload.none(), auth],
 		get: async (request: Request, response: Response) => {
-			const limit: number = request.query.limit
-				? parseInt(request.query.limit as string)
-				: 10
+			if (!mongoose.Types.ObjectId.isValid(request.query.id as string)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid id",
+				})
+			}
 
-			const savedTracksIds = await User.findById(request.body.user._id)
+			const user = await User.findById(request.body.user._id)
 				.select("savedTracks")
 				.populate("savedTracks")
 
-			if (!savedTracksIds)
+			if (!user) {
 				return response.status(400).json({
 					status: "error",
-					message: "No saved albums found",
+					message: "The user was not found",
 				})
+			}
 
-			const albumIds: any[] = savedTracksIds.savedTracks.map(
-				(track: any) => {
-					return track.album
-				}
-			)
+			const albumIds: any[] = user.savedTracks.map((track: any) => {
+				return track.album
+			})
 
 			const contains = albumIds.includes(request.query.id as any)
 
 			response.status(200).json({
 				status: "ok",
-				contains: contains,
+				response: contains,
 			})
 		},
 	}
