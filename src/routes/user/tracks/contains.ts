eca0fed689b564ccdb5d3dd1ef42f183f6d19326
Lucas Täkker <lucastakker@gmail.com>
2024-07-14 16:47:15 +0200

    chore: Refractor user/tracks/contains to be more robust

--- src/routes/user/tracks/contains.ts
+++ src/routes/user/tracks/contains.ts
@@ -1,27 +1,37 @@
 import { Application, Request, Response } from "express"
 import { Resource } from "express-automatic-routes"
-import authenticate from "../../../middleware/auth"
+import auth from "../../../middleware/auth"
 import { User } from "../../../schemas"
+import { upload } from "../../../middleware/upload"
+import mongoose from "mongoose"
 
 export default (express: Application) =>
 	<Resource>{
-		middleware: [authenticate],
+		middleware: [upload.none(), auth],
 		get: async (request: Request, response: Response) => {
+			if (!mongoose.Types.ObjectId.isValid(request.query.id as string)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid id",
+				})
+			}
+
 			const user = await User.findById(request.body.user._id).select(
 				"savedTracks"
 			)
 
-			if (!user)
+			if (!user) {
 				return response.status(400).json({
 					status: "error",
 					message: "The user was not found",
 				})
+			}
 
 			const contains = user.savedTracks.includes(request.query.id as any)
 
 			response.status(200).json({
 				status: "ok",
-				contains: contains,
+				response: contains,
 			})
 		},
 	}
