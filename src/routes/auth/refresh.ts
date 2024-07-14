a17ee9f31439f9e78fb4f1b45c8bd0cd75d37b24
Lucas Täkker <lucastakker@gmail.com>
2024-07-14 17:25:00 +0200

    fix: Updated so it checks if refreshToken is set and is a UUID refractor: Edited return message if the refresh token is invalid

--- src/routes/auth/refresh.ts
+++ src/routes/auth/refresh.ts
@@ -3,20 +3,32 @@ import { Resource } from "express-automatic-routes"
 import { User } from "../../schemas"
 import { v4 as uuidv4 } from "uuid"
 import { upload } from "../../middleware/upload"
+import validator from "validator"
 
 export default (express: Application) =>
 	<Resource>{
 		middleware: [upload.none()],
 		post: async (request: Request, response: Response) => {
+			if (
+				request.body.refreshToken &&
+				!validator.isUUID(request.body.refreshToken)
+			) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid refresh token",
+				})
+			}
+
 			let user = await User.findOne({
 				refreshToken: request.body.refreshToken,
 			})
 
-			if (!user)
+			if (!user) {
 				return response.status(400).json({
 					status: "error",
-					message: "The refresh token is invalid",
+					message: "Invalid refresh token",
 				})
+			}
 
 			user.accessToken = uuidv4()
 			user.accessTokenExpire = new Date(
