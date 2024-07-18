faefdfa60152ceec2ca0dd4d25adb33db7ae7368
Lucas Täkker <lucastakker@gmail.com>
2024-07-18 23:34:38 +0200

    feat: Add route to fetch albums by artist ID

---
+++ src/routes/artists/{id}/albums.ts
@@ -0,0 +1,58 @@
+import { Application, Request, Response } from "express"
+import { Resource } from "express-automatic-routes"
+import { Album, Artist, Track } from "../../../schemas"
+import auth from "../../../middleware/auth"
+import mongoose from "mongoose"
+
+export default (express: Application) =>
+	<Resource>{
+		middleware: [auth],
+		get: async (request: Request, response: Response) => {
+			if (!mongoose.Types.ObjectId.isValid(request.params.id)) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid id",
+				})
+			}
+
+			const limit = request.query.limit
+				? parseInt(request.query.limit as string)
+				: 0
+
+			if (isNaN(limit) || limit < 0) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid limit",
+				})
+			}
+
+			const skip = request.query.offset
+				? parseInt(request.query.offset as string)
+				: 0
+
+			if (isNaN(skip) || skip < 0) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid offset",
+				})
+			}
+
+			const albums = await Album.find({ artists: request.params.id })
+				.limit(limit)
+				.skip(skip)
+				.populate("genres")
+				.populate("artists")
+
+			if (albums.length === 0) {
+				return response.status(404).json({
+					status: "error",
+					message: "No albums found",
+				})
+			}
+
+			response.status(200).json({
+				status: "ok",
+				response: albums,
+			})
+		},
+	}
