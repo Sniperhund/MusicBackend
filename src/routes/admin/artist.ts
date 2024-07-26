d2b55aa1db50afa122746db248f67291ddb61505
Lucas Täkker <lucastakker@gmail.com>
2024-07-26 21:56:10 +0200

    feat: Add PUT route to update artist details and cover feat: Add DELETE route to delete artist

--- src/routes/admin/artist.ts
+++ src/routes/admin/artist.ts
@@ -1,9 +1,11 @@
 import { Application, Request, Response, response } from "express"
 import { Resource } from "express-automatic-routes"
 import { artistCoverUpload } from "../../middleware/upload"
-import { Artist } from "../../schemas"
+import { Album, Artist, Track } from "../../schemas"
 import auth from "../../middleware/auth"
 import cleanFile from "../../utils/cleanFile"
+import mongoose from "mongoose"
+import getFilePath from "../../utils/getFilePath"
 
 export default (express: Application) =>
 	<Resource>{
@@ -49,6 +51,108 @@ export default (express: Application) =>
 				response.status(201).json({
 					status: "ok",
 					response: artist,
+				})
+			},
+		},
+		put: {
+			middleware: [artistCoverUpload.single("file"), auth],
+			handler: async (request: Request, response: Response) => {
+				const fileLocation = request.file?.path as string
+
+				if (request.body.user.role != "admin") {
+					cleanFile(fileLocation)
+
+					return response.status(403).json({
+						status: "error",
+						message: "Unauthorized",
+					})
+				}
+
+				if (
+					!mongoose.Types.ObjectId.isValid(request.query.id as string)
+				) {
+					cleanFile(fileLocation)
+
+					return response.status(400).json({
+						status: "error",
+						message: "Invalid id",
+					})
+				}
+
+				let artist = await Artist.findById(request.query.id)
+
+				if (!artist) {
+					cleanFile(fileLocation)
+
+					return response.status(404).json({
+						status: "error",
+						message: "Artist not found",
+					})
+				}
+
+				if (request.file) {
+					artist.cover = request.file?.filename
+				}
+
+				if (request.body.name) {
+					artist.name = request.body.name
+				}
+
+				await artist.save()
+
+				response.status(200).json({
+					status: "ok",
+					response: artist,
+				})
+			},
+		},
+		delete: {
+			middleware: [auth],
+			handler: async (request: Request, response: Response) => {
+				if (request.body.user.role != "admin") {
+					return response.status(403).json({
+						status: "error",
+						message: "Unauthorized",
+					})
+				}
+
+				if (
+					!mongoose.Types.ObjectId.isValid(request.query.id as string)
+				) {
+					return response.status(400).json({
+						status: "error",
+						message: "Invalid id",
+					})
+				}
+
+				let artist = await Artist.findById(request.query.id)
+
+				if (!artist) {
+					return response.status(404).json({
+						status: "error",
+						message: "Artist not found",
+					})
+				}
+
+				console.log(await Album.find({ artists: artist._id }))
+
+				if (
+					(await Album.find({ artists: artist._id })) ||
+					(await Track.find({ artists: artist._id }))
+				) {
+					return response.status(400).json({
+						status: "error",
+						message: "Artist is in use",
+					})
+				}
+
+				cleanFile(getFilePath("artist", artist.cover as string))
+
+				await Artist.deleteOne({ _id: artist._id })
+
+				response.status(200).json({
+					status: "ok",
+					message: "Artist deleted",
 				})
 			},
 		},
