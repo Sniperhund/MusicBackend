461bfd8c9d4517360258c437c6a502997fd9f70c
Lucas Täkker <lucastakker@gmail.com>
2024-07-26 21:25:22 +0200

    feat: Add PUT route to update album details and cover

--- src/routes/admin/album.ts
+++ src/routes/admin/album.ts
@@ -5,23 +5,27 @@ import { Album } from "../../schemas"
 import auth from "../../middleware/auth"
 import cleanFile from "../../utils/cleanFile"
 import mongoose from "mongoose"
+import getFilePath from "../../utils/getFilePath"
 
 export default (express: Application) =>
 	<Resource>{
 		post: {
 			middleware: [albumCoverUpload.single("file"), auth],
 			handler: async (request: Request, response: Response) => {
+				const fileLocation = request.file?.path as string
+
 				if (request.body.user.role != "admin") {
+					cleanFile(fileLocation)
+
 					return response.status(403).json({
 						status: "error",
 						message: "Unauthorized",
 					})
 				}
 
-				const fileLocation = request.file?.path as string
-
 				if (!request.body.name) {
 					cleanFile(fileLocation)
+
 					return response.status(400).json({
 						status: "error",
 						message: "Name is required",
@@ -32,11 +36,122 @@ export default (express: Application) =>
 				const artist = request.body.artist
 
 				if (!(artists || artist)) {
+					cleanFile(fileLocation)
+
 					return response.status(400).json({
 						status: "error",
 						message: "Artist(s) are required",
 					})
 				}
+
+				if (artists) {
+					if (!Array.isArray(artists)) {
+						cleanFile(fileLocation)
+
+						return response.status(400).json({
+							status: "error",
+							message: "Artists must be an array",
+						})
+					}
+
+					for (let id of artists) {
+						if (!mongoose.Types.ObjectId.isValid(id)) {
+							cleanFile(fileLocation)
+
+							return response.status(400).json({
+								status: "error",
+								message: "Invalid artist",
+							})
+						}
+					}
+				} else {
+					if (!mongoose.Types.ObjectId.isValid(artist)) {
+						cleanFile(fileLocation)
+
+						return response.status(400).json({
+							status: "error",
+							message: "Invalid artist",
+						})
+					}
+
+					artists = [artist]
+				}
+
+				if (!request.file) {
+					cleanFile(fileLocation)
+
+					return response.status(400).json({
+						status: "error",
+						message: "Cover is required",
+					})
+				}
+
+				if (!request.body.genres) {
+					cleanFile(fileLocation)
+
+					return response.status(400).json({
+						status: "error",
+						message: "Genres are required",
+					})
+				}
+
+				let album = new Album({
+					name: request.body.name,
+					artists: artists,
+					cover: request.file?.filename,
+					genres: request.body.genres,
+				})
+
+				await album.save()
+
+				response.status(201).json({
+					status: "ok",
+					response: album,
+				})
+			},
+		},
+		put: {
+			middleware: [albumCoverUpload.single("file"), auth],
+			handler: async (request: Request, response: Response) => {
+				if (request.body.user.role != "admin") {
+					return response.status(403).json({
+						status: "error",
+						message: "Unauthorized",
+					})
+				}
+
+				if (
+					!mongoose.Types.ObjectId.isValid(request.query.id as string)
+				) {
+					return response.status(400).json({
+						status: "error",
+						message: "Invalid id",
+					})
+				}
+
+				const album = await Album.findById(request.query.id)
+
+				if (!album) {
+					return response.status(404).json({
+						status: "error",
+						message: "Album not found",
+					})
+				}
+
+				const newFileLocation = request.file?.path as string
+
+				if (newFileLocation) {
+					const oldFileLocation = album.cover
+
+					album.cover = request.file?.filename
+
+					cleanFile(getFilePath("album", oldFileLocation as string))
+				}
+
+				if (request.body.name) album.name = request.body.name
+
+				let artists = request.body.artists
+				const artist = request.body.artist
 
 				if (artists) {
 					if (!Array.isArray(artists)) {
@@ -65,34 +180,19 @@ export default (express: Application) =>
 					artists = [artist]
 				}
 
-				if (!request.file) {
-					cleanFile(fileLocation)
-					return response.status(400).json({
-						status: "error",
-						message: "Cover is required",
-					})
-				}
+				if (artists) album.artists = artists
 
-				if (!request.body.genres) {
-					cleanFile(fileLocation)
-					return response.status(400).json({
-						status: "error",
-						message: "Genres are required",
-					})
-				}
-
-				let album = new Album({
-					name: request.body.name,
-					artists: artists,
-					cover: request.file?.filename,
-					genres: request.body.genres,
-				})
+				if (request.body.genres) album.genres = request.body.genres
 
 				await album.save()
 
-				response.status(201).json({
+				const updatedAlbum = await Album.findById(album._id)
+					.populate("artists")
+					.populate("genres")
+
+				response.status(200).json({
 					status: "ok",
-					response: album,
+					response: updatedAlbum,
 				})
 			},
 		},
