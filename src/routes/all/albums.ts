79d76754b1f5cb8be73dc371c4024c76ab2cf550
Lucas Täkker <lucastakker@gmail.com>
2024-07-27 00:49:12 +0200

    feat: Add pagination to all routes for albums, artists, genres, and tracks

--- src/routes/all/albums.ts
+++ src/routes/all/albums.ts
@@ -8,9 +8,33 @@ export default (express: Application) =>
 	<Resource>{
 		middleware: [upload.none(), auth],
 		get: async (request: Request, response: Response) => {
+			const limit = request.query.limit
+				? parseInt(request.query.limit as string)
+				: 0
+
+			if (isNaN(limit) || limit < 0) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid limit",
+				})
+			}
+
+			const skip = request.query.offset
+				? parseInt(request.query.offset as string)
+				: 0
+
+			if (isNaN(skip) || skip < 0) {
+				return response.status(400).json({
+					status: "error",
+					message: "Invalid offset",
+				})
+			}
+
 			const albums = await Album.find({})
 				.populate("artists")
 				.populate("genres")
+				.limit(limit)
+				.skip(skip)
 
 			if (!albums) {
 				return response.status(404).json({
