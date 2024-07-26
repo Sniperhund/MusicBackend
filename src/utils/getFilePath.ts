efe5bc330b02bddc42220792d45b347d7ff08209
Lucas Täkker <lucastakker@gmail.com>
2024-07-26 21:17:14 +0200

    fix: Fixed small typo

--- src/utils/getFilePath.ts
+++ src/utils/getFilePath.ts
@@ -4,13 +4,13 @@ export default function getFilePath(type: String, path: String) {
 	switch (type) {
 		case "album":
 		case "Album":
-			return `${backendUrl}albumCover/${path}`
+			return `${uploadDir}albumCover/${path}`
 		case "artist":
 		case "Artist":
-			return `${backendUrl}artistCover/${path}`
+			return `${uploadDir}artistCover/${path}`
 		case "track":
 		case "Track":
 		default:
-			return `${backendUrl}track/${path}`
+			return `${uploadDir}track/${path}`
 	}
 }
