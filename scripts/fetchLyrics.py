56d1b78d97666305d8bfcd98beaa841a556c49d0
Lucas Täkker <lucastakker@gmail.com>
2024-11-04 19:59:18 +0100

    feat: Enhance lyrics fetching script with user confirmation and search functionality

--- scripts/fetchLyrics.py
+++ scripts/fetchLyrics.py
@@ -1,5 +1,6 @@
 import syncedlyrics, time, sys
 from util import *
+from colorama import Fore, Style
 
 api = "https://api.lucasskt.dk/"
 
@@ -14,7 +15,6 @@ def checkIfSongHasLyrics(songId):
         return True, songLyrics.json().get("response").get("synced")
     return False
 
-
 def getSearchTerm(song):
     artistNames = ""
 
@@ -23,12 +23,11 @@ def getSearchTerm(song):
 
     return song.get("name") + " " + artistNames
 
-
 def addLyrics():
     songs = session.get("all/tracks").json().get("response")
     for song in songs:
         addSpecificLyrics(song.get("_id"))
-
+        time.sleep(timeBetweenRequests)
 
 def addSpecificLyrics(songId):
     song = session.get("tracks/" + songId).json().get("response")
@@ -40,14 +39,19 @@ def addSpecificLyrics(songId):
 
             lrc = getLyrics(getSearchTerm(song))
 
-            response = session.put("/admin/lyrics?id=" + song.get("_id"),
-                                   json={"synced": True, "lyrics": lrc})
+            if lrc == None:
+                print("No lyrics found")
+                return
 
-            if response.status_code != 200:
-                print(song)
-                print(lrc)
-                print(response.json())
-                raise Exception("Something went wrong")
+            if confirmLyrics(song, lrc):
+                response = session.put("/admin/lyrics?id=" + song.get("_id"),
+                                    json={"synced": True, "lyrics": lrc})
+
+                if response.status_code != 200:
+                    print(song)
+                    print(lrc)
+                    print(response.json())
+                    raise Exception("Something went wrong")
         else:
             print("Already synced")
     else:
@@ -55,26 +59,81 @@ def addSpecificLyrics(songId):
 
         lrc = getLyrics(getSearchTerm(song))
 
-        response = session.post("/admin/lyrics",
+        if lrc == None:
+            print("No lyrics found")
+            return
+
+        if confirmLyrics(song, lrc):
+            response = session.post("/admin/lyrics",
                                 json={"songId": song.get("_id"), "synced": True, "lyrics": lrc})
 
-        if response.status_code != 201:
-            print(song)
-            print(lrc)
-            print(response.json())
-            raise Exception("Something went wrong")
-    time.sleep(timeBetweenRequests)
+            if response.status_code != 201:
+                print(song)
+                print(lrc)
+                print(response.json())
+                raise Exception("Something went wrong")
 
+def formatString(s):
+    return s.replace("\\n", "\n").replace("\\t", "\t")
+
+def confirmLyrics(song, lrc):
+    print(Fore.RED + "Lyrics:" + Style.RESET_ALL)
+    print(formatString(lrc))
+    print(Fore.RED + "Song information (on the backend):" + Style.RESET_ALL)
+    print(Fore.GREEN + "Name: " + Style.RESET_ALL + song.get("name"))
+    print(Fore.GREEN + "Artists: " + Style.RESET_ALL + ", ".join([artist.get("name") for artist in song.get("artists")]))
+    print(Fore.GREEN + "Album: " + Style.RESET_ALL + song.get("album").get("name"))
+
+    print("Do you want to add these lyrics? (y/n)")
+    answer = input()
+
+    if answer.lower() == "y":
+        return True
+    
+    return False
+
+def searchSong():
+    searchTerm = input("Enter a search term: ")
+
+    response = session.get("/search?q=" + searchTerm)
+
+    if response.status_code != 200:
+        print(response.json())
+        raise Exception("Something went wrong")
+
+    songs = response.json().get("response")
+
+    if len(songs) == 0:
+        print("No songs found")
+        return
+
+    for song in songs:
+        if song.get("type") != "track":
+            songs.remove(song)
+
+    for i, song in enumerate(songs):
+        print(str(i) + ": " + song.get("name") + " by " + ", ".join([artist.get("name") for artist in song.get("artists")]))
+
+    songIndex = int(input("Select a song by typing the number: "))
+
+    addSpecificLyrics(songs[songIndex].get("_id"))
 
 def getLyrics(searchTerm):
     lrc = syncedlyrics.search(searchTerm)
     return lrc
 
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        addSpecificLyrics(sys.argv[1])
+    if len(sys.argv) == 1:
+        print(Fore.RED + "Missing arguments" + Style.RESET_ALL)
+        print("Usage: python fetchLyrics.py --id <songId>")
+        print("Usage: python fetchLyrics.py --search")
     else:
-        print("For the time being, this script only supports adding lyrics to a specific song by providing the song id as an argument")
-        print("Since it sometimes gets the wrong song, it is recommended to use the admin panel to add lyrics")
-        #addLyrics()
+        match sys.argv[1]:
+            case "--id":
+                addSpecificLyrics(sys.argv[2])
+            case "--search":
+                searchSong()
+            case _:
+                print(Fore.RED + "Invalid argument" + Style.RESET_ALL)
+                print("Usage: python fetchLyrics.py --id <songId>")
+                print("Usage: python fetchLyrics.py --search")
