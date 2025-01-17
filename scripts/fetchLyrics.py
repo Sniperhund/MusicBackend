fed4aedbff203d2dbc899442c906899c10c16f9b
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2025-01-17 01:24:09 +0100

    feat: Add environment variable checks and signin functionality for lyrics fetching

--- scripts/fetchLyrics.py
+++ scripts/fetchLyrics.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python3
 
-import syncedlyrics, time, sys
+import syncedlyrics, time, sys, os
 from util import *
 from colorama import Fore, Style
+from dotenv import load_dotenv
 
 api = "https://api.lucasskt.dk/"
 
 session = LiveServerSession(api)
 timeBetweenRequests = 30
 
+load_dotenv()
 
 def checkIfSongHasLyrics(songId):
     songLyrics = session.get("/tracks/" + songId + "/lyrics")
@@ -125,6 +127,19 @@ def getLyrics(searchTerm):
     return lrc
 
 if __name__ == "__main__":
+    if os.getenv("REFRESH_TOKEN") == None and os.getenv("SIGNIN_INSTEAD") == None or os.getenv("SIGNIN_INSTEAD") == "true" and (os.getenv("EMAIL") == None or os.getenv("PASSWORD") == None):
+        print(Fore.RED + "Missing environment variables" + Style.RESET_ALL)
+        print(Fore.GREEN + "You must either have a .env file in the root of the project with the following variables:" + Style.RESET_ALL)
+        print("REFRESH_TOKEN")
+        print(Fore.BLUE + "You can get the refresh token by signing in to the website and copying the token from the cookies" + Style.RESET_ALL)
+        print(Fore.GREEN + "\nIf you want to sign in instead, you must have the following variables:" + Style.RESET_ALL)
+        print("SIGNIN_INSTEAD=true")
+        print("EMAIL")
+        print("PASSWORD")
+        print(Fore.RED + "\nNever share you're .env file with anyone as it gives full access to your account with any of these values" + Style.RESET_ALL)
+
+        exit()
+
     if len(sys.argv) == 1:
         print(Fore.RED + "Missing arguments" + Style.RESET_ALL)
         print("Usage: python fetchLyrics.py --id <songId>")
--- scripts/util.py
+++ scripts/util.py
@@ -12,7 +12,13 @@ class LiveServerSession(Session):
         super().__init__()
         self.base_url = base_url
 
-        self.refresh_token()
+        if os.getenv("REFRESH_TOKEN") == None and os.getenv("SIGNIN_INSTEAD") == None or os.getenv("SIGNIN_INSTEAD") == "true" and (os.getenv("EMAIL") == None or os.getenv("PASSWORD") == None):
+            return
+
+        if os.getenv("SIGNIN_INSTEAD") == "true":
+            self.signin()
+        else:
+            self.refresh_token()
 
     def refresh_token(self):
         global accessToken
@@ -28,6 +34,22 @@ class LiveServerSession(Session):
             accessToken = response.json()['response']['accessToken']
         else:
             raise Exception('Failed to refresh token')
+
+    def signin(self):
+        global accessToken
+        url = urljoin(self.base_url, 'auth/login')
+
+        body = {
+            'email': os.getenv('EMAIL'),
+            'password': os.getenv('PASSWORD')
+        }
+
+        response = self.post(url, json=body)
+
+        if response.status_code == 200:
+            accessToken = response.json()['response']['accessToken']
+        else:
+            raise Exception('Failed to sign in')
 
     def request(self, method, url, *args, **kwargs):
         joined_url = urljoin(self.base_url, url)
